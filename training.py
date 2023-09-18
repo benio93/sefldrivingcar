ced1d55012ef25072e6bd52e2037e3f0aabd7cf9
unknown <beniaminsawczak21@gmail.com>
2023-09-18 18:47:45 +0200

    second commit

--- training.py
+++ training.py
@@ -6,3 +6,42 @@ Created on Mon Sep 18 17:49:38 2023
 """
 
 print("hello world")
+
+# AI for Self Driving Car
+
+# Importing the libraries
+
+import numpy as np
+import random
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.autograd as autograd
+from torch.autograd import Variable
+
+# Creating the architecture of the Neural Network
+
+class Network(nn.Module):
+    
+    def __init__(self, input_size, nb_action):
+        super(Network, self).__init__()
+        self.input_size = input_size
+        self.nb_action = nb_action
+        self.fc1 = nn.Linear(input_size, 30)
+        self.fc2 = nn.Linear(30, nb_action)
+    
+    def forward(self, state):
+        x = F.relu(self.fc1(state))
+        q_values = self.fc2(x)
+        return q_values
+
+# Implementing Experience Replay
+
+class ReplayMemory(object):
+    
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+    
